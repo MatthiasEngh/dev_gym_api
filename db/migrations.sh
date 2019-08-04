70944c4dfb6e02815927722336b0bf0b06b64f10
M E <matthias.engh@gmail.com>
2019-08-04 16:09:54 -0500

    change initial migrations

--- db/migrations.sh
+++ db/migrations.sh
@@ -1,7 +1,16 @@
 database="dev_gym_api"
 
-psql -d $database -c "CREATE TABLE match(
+psql -d $database -c "CREATE TABLE users(
+  user_id uuid NOT NULL DEFAULT uuid_generate_v1()
+)"
+
+psql -d $database -c "CREATE TABLE matches(
   match_id uuid NOT NULL DEFAULT uuid_generate_v1(),
-  game_id uuid,
-  open boolean
+  game_id uuid NOT NULL,
+  open boolean NOT NULL DEFAULT TRUE
+)"
+
+psql -d $database -c "CREATE TABLE match_enrollments(
+  match_id uuid NOT NULL,
+  user_id uuid NOT NULL
 )"
