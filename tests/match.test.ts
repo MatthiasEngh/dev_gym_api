c5b00a402882e904fb7e3b350b0dd35f49b67109
M E <matthias.engh@gmail.com>
2019-08-05 01:29:45 -0500

    expect matchmaking

--- tests/match.test.ts
+++ tests/match.test.ts
@@ -2,8 +2,15 @@ const http = require('http')
 const axios = require('axios')
 
 // expect the match route to exist
-test('route /match exists', async () => {
-  return axios.get(
+test('route can be used to setup a game', async () => {
+  await axios.get(
     'http://127.0.0.1:3000/match'
-  )
+  ).then(response => {
+    expect(response.status).toEqual(204)
+  })
+  await axios.get(
+    'http://127.0.0.1:3000/match'
+  ).then(response => {
+    expect(response.status).toEqual(200)
+  })
 })
