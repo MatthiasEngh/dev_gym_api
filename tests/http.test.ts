9289534ff7a5d6ea52a373cadf83fdb42b6ff18f
M E <matthias.engh@gmail.com>
2019-08-05 00:45:39 -0500

    add route /match

--- tests/http.test.ts
+++ tests/http.test.ts
@@ -1,3 +1,11 @@
-test('test suite', async () => {
-  expect(true).toEqual(true)
+const http = require('http')
+const axios = require('axios')
+
+// expect the match route to exist
+test('route /match exists', async () => {
+  axios.get(
+    '127.0.0.1:3000/match'
+  ).then( response => {
+    expect(response.status).toEqual(200)
+  })
 })
