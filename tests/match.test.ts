81843d09d2d3cc35f0531337314035a855388a2c
M E <matthias.engh@gmail.com>
2019-08-05 01:22:29 -0500

    make axios request work

--- tests/match.test.ts
+++ tests/match.test.ts
@@ -3,9 +3,7 @@ const axios = require('axios')
 
 // expect the match route to exist
 test('route /match exists', async () => {
-  axios.get(
-    '127.0.0.1:3000/match'
-  ).then( response => {
-    expect(response.status).not.toEqual(404)
-  })
+  return axios.get(
+    'http://127.0.0.1:3000/match'
+  )
 })
