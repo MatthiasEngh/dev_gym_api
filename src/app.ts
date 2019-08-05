48ed352106b8186cb22a896027bd97bb78f9f770
M E <matthias.engh@gmail.com>
2019-08-04 23:46:56 -0500

    routing tweaks

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,14 @@ const port = 3000;
 const server = http.createServer((req: any, res: any) => {
   let path: string = url.parse(req.url).pathname
 
-  if (/game/i.test(path)) {
+  if (/^\/game/i.test(path)) {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('game');
   } else {
     res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('NO MANS LAND, go to /game');
   }
 });
 
