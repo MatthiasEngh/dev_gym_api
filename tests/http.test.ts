a0c751a3e57caa9e9a7094a83e81cee6b78170b6
Brice Maltby <brice.maltby@gmail.com>
2019-08-05 20:01:22 -0500

    Fix lack of protocol in http tests

--- tests/http.test.ts
+++ tests/http.test.ts
@@ -1,29 +1,29 @@
 const http = require('http')
 const axios = require('axios')
 
-
-//require('./') // start server // TODO: does below line work?
+// TODO: does below line work?
+//require('./') // start server 
 test('bad auth', async () => {
   const response = await axios.get(
-    '127.0.0.1:3000',
+    'http://127.0.0.1:3000',
     {
       auth: {
-      username: 'fake',
-      password: 'bad'
-    }
-  })
+	username: 'fake',
+	password: 'bad'
+      }
+    })
   expect(response.status).toEqual(400)
 })
 
 test('good auth', async () => {
   const response = await axios.get(
-    '127.0.0.1:3000',
+    'http://127.0.0.1:3000',
     {
       auth: {
-      username: 'username',
-      password: 'password'
-    }
-  })
+	username: 'username',
+	password: 'password'
+      }
+    })
   expect(response.status).toEqual(200)
   expect(response.data.token).toBeTruthy()
 })
@@ -33,34 +33,34 @@ test('matchmaking', async () => {
 
   // Wrong game key
   const badGameKeyResponse = await axios.get(
-  '127.0.0.1:3000/nope',
-  {
-    headers: {
-      Authorization: playerOneToken
-    }
-  })
+    'http://127.0.0.1:3000/nope',
+    {
+      headers: {
+	Authorization: playerOneToken
+      }
+    })
   expect(badGameKeyResponse.status).toEqual(404) // 404 == Not found
 
   // No match yet configured
   const firstMatchmakingResponse = await axios.get(
-  '127.0.0.1:3000/game1',
-  {
-    headers: {
-      Authorization: playerOneToken
-    }
-  })
+    'http://127.0.0.1:3000/game1',
+    {
+      headers: {
+	Authorization: playerOneToken
+      }
+    })
   expect(firstMatchmakingResponse.status).toEqual(204) // 204 == No content
   expect(firstMatchmakingResponse.data).toBeNull()
 
   // Create 2nd player, still no match
   const playerTwoToken = getAuthToken()
   const secondMatchmakingResponse = await axios.get(
-  '127.0.0.1:3000/game1',
-  {
-    headers: {
-      Authorization: playerTwoToken
-    }
-  })
+    'http://127.0.0.1:3000/game1',
+    {
+      headers: {
+	Authorization: playerTwoToken
+      }
+    })
   expect(secondMatchmakingResponse.status).toEqual(200)
   expect(secondMatchmakingResponse.data).toEqual(
     {} // TODO: expected match response goes here
@@ -68,12 +68,12 @@ test('matchmaking', async () => {
 
   // Retry w/ player one, should receive same game as player two
   const thirdMatchmakingResponse = await axios.get(
-  '127.0.0.1:3000/game1',
-  {
-    headers: {
-      Authorization: playerOneToken
-    }
-  })
+    'http://127.0.0.1:3000/game1',
+    {
+      headers: {
+	Authorization: playerOneToken
+      }
+    })
   expect(secondMatchmakingResponse.status).toEqual(200)
   expect(secondMatchmakingResponse.data).toEqual(
     {} // TODO: expected match response goes here
@@ -82,13 +82,13 @@ test('matchmaking', async () => {
 
 async function getAuthToken() { // TODO: does this return a new user each time?
   const authResponse = await axios.get(
-  	'127.0.0.1:3000',
-  	{
-	    auth: {
-    		username: 'username',
-    		password: 'password'
+    'http://127.0.0.1:3000',
+    {
+      auth: {
+	username: 'username',
+	password: 'password'
       }
-	})
+    })
   expect(authResponse.status).toEqual(200)
   expect(authResponse.data.token).toBeTruthy()
   return authResponse.data.token
