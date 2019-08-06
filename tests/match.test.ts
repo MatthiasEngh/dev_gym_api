9c3e47e4c7ef65bf324ee3311027a768cb3cf27b
M E <matthias.engh@gmail.com>
2019-08-05 19:29:14 -0500

    set up new expectations

--- tests/match.test.ts
+++ tests/match.test.ts
@@ -1,15 +1,32 @@
 const http = require('http')
 const axios = require('axios')
 
-// expect the match route to exist
 test('route can be used to setup a game', async () => {
+  // first: one user connects, no match
   await axios.get(
-    'http://127.0.0.1:3000/match'
+    'http://127.0.0.1:3000/match',
+    { player_id: "some_id1" }
   ).then(response => {
     expect(response.status).toEqual(204)
   })
+  // second: first user reconnects, still no match
   await axios.get(
-    'http://127.0.0.1:3000/match'
+    'http://127.0.0.1:3000/match',
+    { player_id: "some_id1" }
+  ).then(response => {
+    expect(response.status).toEqual(204)
+  })
+  // third: second user connects, match
+  await axios.get(
+    'http://127.0.0.1:3000/match',
+    { player_id: "some_id2" }
+  ).then(response => {
+    expect(response.status).toEqual(200)
+  })
+  // fourth: first user reconnects, match
+  await axios.get(
+    'http://127.0.0.1:3000/match',
+    { player_id: "some_id1" }
   ).then(response => {
     expect(response.status).toEqual(200)
   })
