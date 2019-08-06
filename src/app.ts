6d5cde78658b4ee03091b764e81329c351facf68
M E <matthias.engh@gmail.com>
2019-08-05 19:24:54 -0500

    move stuff around

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,17 @@ const http = require('http');
 const url = require('url');
 const { Client } = require('pg')
 
+const hostname = '127.0.0.1';
+const db_host = hostname
+const port = 3000;
+
 const client = new Client({
   user: 'dev_gym_api',
-  host: '127.0.0.1',
+  host: db_host,
   database: 'dev_gym_api',
   port: '5432',
 })
 client.connect()
-
-const hostname = '127.0.0.1';
-const port = 3000;
 
 const server = http.createServer(async (req: any, res: any) => {
   let path: string = url.parse(req.url).pathname
