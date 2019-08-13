b3e2f6101a7276b50e526cc4a3f405a56d09fa6d
Brice Maltby <brice.maltby@gmail.com>
2019-08-12 20:26:13 -0500

    Add basic input interface

---
+++ games/islands/interfaces.ts
@@ -0,0 +1,4 @@
+export interface MoveData {
+  player: string
+  moves: Array<number>
+}
---
+++ games/islands/islands.ts
@@ -0,0 +1,28 @@
+// NOTE: object descriptions are subject to change
+// input:
+// [
+//   {
+//     player: 'mat'
+//     input: [
+//       5,
+//       7,
+//       0,
+//       0,
+//       0,
+//       32,
+//       ...
+//     ]
+//   },
+//   {
+//     player: 'brice'
+//     input: [
+//       ...
+//     ]
+//   }
+// ]
+// player input: [1,5,0,0,3,0,4,4,7,0,22,0,...]
+// player receives: [(0,0,),(1,7),(0,0),(0,0),(0,0),(2,31),...]
+
+function main(input: MoveData) {
+}
+  
