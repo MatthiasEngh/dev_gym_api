245340ac19d54d242ef6ce651c83b99731eed84e
Brice Maltby <brice.maltby@gmail.com>
2019-08-14 21:22:43 -0500

    Define main island logix

--- games/islands/islands.ts
+++ games/islands/islands.ts
@@ -40,13 +40,21 @@
 // }
 
 
-function nodeResult(prevOwnership: number, playerOneSoldiers: number, playerTwoSoldiers: number): number {
+export function nodeResult(prevOwnership: number, playerOneSoldiers: number, playerTwoSoldiers: number): [number, number] {
   const pOne = playerOneSoldiers
   const pTwo = playerTwoSoldiers
+  const newSoldierCount = Math.max(pOne, pTwo) + 1
   if (pOne > pTwo)
-    return 1
+    return [1, newSoldierCount]
   else if (pTwo > pOne)
-    return 2
-  else
-    return prevOwnership
+    return [2, newSoldierCount]
+  else if ((pTwo == pOne) && (prevOwnership))
+    return [prevOwnership, newSoldierCount]
+  else if ((pTwo == pOne) && !(prevOwnership))
+    return [prevOwnership, 0]
+  // no soldiers moved to node
+  else if (prevOwnership)
+    return [prevOwnership, 1] 
+  else // no ownership, no soldiers awarded
+    return [prevOwnership, 0]
 }
