8d5788805d967a56bf0bb8f4d55307eaeaebf8ba
Brice Maltby <brice.maltby@gmail.com>
2019-08-14 19:54:43 -0500

    Document island io

--- games/islands/islands.ts
+++ games/islands/islands.ts
@@ -1,27 +1,44 @@
-// NOTE: object descriptions are subject to change
-// input:
+// game start:
 // [
 //   {
-//     player: 'mat'
-//     input: [
-//       5,
-//       7,
-//       0,
-//       0,
-//       0,
-//       32,
-//       ...
-//     ]
+//     playerUUID: 'matthias',
+//     playerNumber: 1
 //   },
 //   {
-//     player: 'brice'
-//     input: [
-//       ...
-//     ]
+//     playerUUID: 'brice',
+//     playerNumber: 2
 //   }
 // ]
-// player input: [1,5,0,0,3,0,4,4,7,0,22,0,...]
-// player receives: [(0,0,),(1,7),(0,0),(0,0),(0,0),(2,31),...]
+// 
+// player input:
+// [ 2, 4, 4, 42, 1, 0, 0, 23, 0, ... ]
+// 
+// game move returns:
+// {
+//   "islands":
+//   [
+//     {
+//       "ownership": 2,  
+//       "playerOneSoldiers": 2,
+//       "playerTwoSoldiers": 5
+//     },
+//     {
+//       "ownership": 0,  
+//       "playerOneSoldiers": 2,
+//       "playerTwoSoldiers": 2
+//     },
+//     {
+//       "ownership": 1,
+//       "playerOneSoldiers": 0,
+//       "playerTwoSoldiers": 0
+//     }
+//   ]
+//   "totalSoldiers": {
+//     "playerOne": 123,
+//     "playerTwo": 63
+//   }
+// }
+
 
 function main(input: MoveData) {
 }
